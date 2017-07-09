b7ee924ce5df427fef2de91ebb690fe2b9a16e43
Mark Hansen <mark@markhansen.co.nz>
2017-07-09 17:25:43 +1000

    Remove serviceworker code. I don't understand it, add it back maybe one day later.

--- site2/src/index.js
+++ site2/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap http://stackoverflow.com/a/34015469/988941
