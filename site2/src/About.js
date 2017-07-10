8ca3e684852f52a838ba906046e0291dfcdc5fb9
Mark Hansen <mark@markhansen.co.nz>
2017-07-10 20:55:22 +1000

    extend react.component. it's a bit clearer

--- site2/src/About.js
+++ site2/src/About.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class DrawerContent extends Component {
+export default class DrawerContent extends React.Component {
   render() {
     return (<div>
       <h2>About</h2>
