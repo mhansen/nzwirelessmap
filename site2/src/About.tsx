371218e79f60af00d6f8bd3175ebbf4ac1a368ff
Mark Hansen <mark@markhansen.co.nz>
2020-02-09 11:20:05 +1100

    Update about page

--- site2/src/About.tsx
+++ site2/src/About.tsx
@@ -4,13 +4,14 @@ export default class DrawerContent extends React.Component {
   render() {
     return (<div>
       <p>Each line is a point-to-point wireless radio link registered with 
-      the NZ Government <a href="https://www.rsm.govt.nz/">Radio Spectrum Management</a> team.</p>
+      the NZ Government <a href="https://www.rsm.govt.nz/">Radio Spectrum Management</a> team.
+      </p>
       <p>Click lines to see details about the radio link.</p>
       <p>
-      Thanks to <a href="http://www.rsm.govt.nz/cms/tools-and-services/spectrum-search-lite">Radio
+      Thanks to <a href="https://www.rsm.govt.nz/engineers-and-examiners/resources-for-engineers-and-certifiers/spectrum-search-lite/">Radio
         Spectrum Management</a> who make the database available.
-         The data is <a href="http://www.rsm.govt.nz/about-this-site/copyright-c/">Crown
-        Copyright</a>, with permission to reproduce accurately in any format.</p>
+        The data is <a href="https://www.rsm.govt.nz/copyright/">Crown Copyright</a>.
+      </p>
       <p>
         This site was made by <a href="//markhansen.co.nz/">Mark Hansen</a>.
         The code's on <a href="https://github.com/mhansen/nzwirelessmap">GitHub</a>.
@@ -19,7 +20,6 @@ export default class DrawerContent extends React.Component {
         <a href="http://markhansen.co.nz/nz-wireless-map">How this was made</a>.
       </p>
       <p>If you have any feedback, <a href="mailto:mark@markhansen.co.nz">email Mark</a>.</p>
-      <p>If you'd like more powerful filtering, try <a href="https://fusiontables.google.com/DataSource?docid=1fAeKubYzWae7KT2qYFzku1M6N3c1Gncs1XDgPOc#rows:id=1">Google Fusion Tables on the raw data</a> behind this site.</p>
       <p>You may also enjoy <a href="https://broadbandmap.nz/">National Broadband Map</a>.</p>
     </div>)
   }
