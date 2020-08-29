ac43ebbca758a3bca962551ec381f15a33e2d077
Mark Hansen <mark@markhansen.co.nz>
2020-08-29 12:33:40 +1000

    Add commit, telling people not to burn down 5G towers
    Sad it's come to this

--- site2/src/About.tsx
+++ site2/src/About.tsx
@@ -29,6 +29,20 @@ export default class DrawerContent extends React.Component<IProps, IState> {
       </p>
       <p>If you have any feedback, <a href="mailto:mark@markhansen.co.nz">email Mark</a>.</p>
       <p>You may also enjoy <a href="https://broadbandmap.nz/">National Broadband Map</a>.</p>
+      <p>
+        Please don't use this to burn down 5G towers. Our best evidence is that 5G is safe:
+        <ul>
+          <li>
+            "As I scientist, I think it's healthy to be skeptical of current knowledge. but is there any reason to be worried about 5G? the simple answer is no- If there is if there is a harmful effect, I honestly think we’d know about it by now." -- <a href="https://docs.google.com/document/d/e/2PACX-1vR0Ik4UveZytN1tVQYmIY5NIGbCkUYfytmBDatPRvPzjGLFN6Nil5Wdl26TosUNz_L6TPfTeU5mxuqF/pub#ftnt_ref95">Chris Collins, a professor of radiology at NYU School of medicine, on "Science VS" podcast.</a>
+          </li>
+          <li>
+            <a href="https://www.who.int/news-room/fact-sheets/detail/electromagnetic-fields-and-public-health-mobile-phones">
+              World Health Organization:
+            </a>{' '}
+            "A large number of studies have been performed over the last two decades to assess whether mobile phones pose a potential health risk. To date, no adverse health effects have been established as being caused by mobile phone use."
+          </li>
+        </ul>
+      </p>
     </div>)
   }
 }
